aa103da049c0c7a24e818c68336824f863ebf032
Raphael Van Well <rapha.well@outlook.com>
2023-01-01 12:11:52 -0300

    Created Domain with class Product

--- src/Domain/Products/Category.cs
+++ src/Domain/Products/Category.cs
@@ -1,8 +1,11 @@
-﻿namespace IWantAPI.Domain.Products
+﻿namespace IWantAPI.Domain.Products;
+
+public class Category
 {
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public string EditedBy { get; set; }
+    public DateTime EditedOn { get; set;}
 }
