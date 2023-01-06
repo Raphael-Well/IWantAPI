296c8b1dc6aa6cbbff7178ba784ec46eb4047451
Raphael Van Well <rapha.well@outlook.com>
2023-01-05 23:48:14 -0300

    Identity Create

---
+++ src/Endpoints/Employees/EmployeeRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace IWantAPI.Endpoints.Employees;
+
+public record EmployeeRequest(
+    string Email,
+    string Password,
+    string Name,
+    string EmployeeCode);
--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,19 @@
 using IWantAPI.Endpoints.Categories;
+using IWantAPI.Endpoints.Employees;
 using IWantAPI.Infra.Data;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["ConnectionString:IWantDb"]);
-
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit= false;
+    options.Password.RequireUppercase= false;
+    options.Password.RequiredLength= 3;
+    options.Password.RequireLowercase= false;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,6 +32,7 @@ app.UseHttpsRedirection();
 app.MapMethods(CategoryPost.Template, CategoryPost.Methods, CategoryPost.Handle);
 app.MapMethods(CategoryGetAll.Template, CategoryGetAll.Methods, CategoryGetAll.Handle);
 app.MapMethods(CategoryPut.Template, CategoryPut.Methods, CategoryPut.Handle);
+app.MapMethods(EmployeePost.Template, EmployeePost.Methods, EmployeePost.Handle);
 
 app.Run();
 
